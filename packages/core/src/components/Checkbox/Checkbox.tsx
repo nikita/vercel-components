5b658c12e6f8b9061b1eb98fb42a5600bbc094d1
никита <23018519+nikita@users.noreply.github.com>
2022-06-26 22:26:10 -0700

    refactor(Checkbox): match vercel

--- packages/core/src/components/Checkbox/Checkbox.tsx
+++ packages/core/src/components/Checkbox/Checkbox.tsx
@@ -1,68 +1,109 @@
-import React from "react";
+import React, { FC, memo } from "react";
 import clsx from "clsx";
-import { InputHTMLAttributes } from "react";
+import { useId } from "@react-aria/utils";
+import { Label } from "../Label";
+import { useDisabled } from "../../contexts/DisabledContext";
 import styles from "./Checkbox.module.css";
 
+const CheckedIcon = () => (
+  <path
+    d="M14 7L8.5 12.5L6 10"
+    stroke="var(--geist-background)"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    strokeWidth="2"
+  />
+);
+
+const IndeterminateIcon = () => (
+  <line
+    stroke="var(--checkbox-color)"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    strokeWidth="2"
+    x1="5"
+    x2="15"
+    y1="10"
+    y2="10"
+  />
+);
+
 interface Props {
-  id?: string;
-  checked?: boolean;
+  label?: string;
   disabled?: boolean;
-  indeterminate?: boolean;
+  checked?: boolean;
+  className?: any;
   children?: string | JSX.Element;
-  onChange?: InputHTMLAttributes<HTMLInputElement>["onChange"];
+  indeterminate?: boolean;
   style?: React.CSSProperties;
+  fullWidth?: boolean;
+
+  onChange?: any;
 }
-const Checkbox: React.ComponentType<Props> = ({
-  id,
-  checked,
+
+const Checkbox: FC<Props> = ({
+  label,
   disabled,
-  indeterminate,
-  onChange,
+  checked = false,
+  className,
   children,
+  indeterminate,
   style,
+  fullWidth,
+  ...props
 }) => {
+  const checkboxId = "select-" + useId();
+  const ctxDisabled = useDisabled() || disabled;
+
+  const CheckboxContainer = label ? Label : React.Fragment;
+  const ElementType = label === undefined ? "label" : "span";
+
   return (
-    <label
-      className={clsx(styles.container, { [styles.disabled]: disabled })}
-      style={style}
+    <CheckboxContainer
+      {...(label
+        ? {
+            value: label,
+            withInput: true,
+            id: checkboxId,
+          }
+        : undefined)}
     >
-      <span className={styles.check}>
-        <input
-          checked={checked}
-          disabled={disabled}
-          onChange={onChange}
-          className={clsx(styles["geist-sr-only"], styles.input)}
-          type="checkbox"
-          id={id}
-        />
-        <span className={styles.icon}>
-          <svg viewBox="0 0 20 20" height="16" width="16" fill="none">
-            {indeterminate ? (
-              <line
-                x1="5"
-                y1="10"
-                x2="15"
-                y2="10"
-                stroke="var(--checkbox-color)"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              ></line>
-            ) : (
-              <path
-                d="M14 7L8.5 12.5L6 10"
-                stroke="var(--geist-background)"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              ></path>
-            )}
-          </svg>
+      <ElementType
+        className={clsx(styles.container, className, {
+          [styles.disabled]: disabled,
+          [styles.fullWidth]: fullWidth,
+        })}
+        style={style}
+      >
+        <span className={styles.check}>
+          {children && <React.Fragment>{"\u200b"}</React.Fragment>}
+          <input
+            {...props}
+            checked={checked}
+            className={clsx("geist-sr-only", styles.input)}
+            disabled={ctxDisabled}
+            id={checkboxId}
+            type="checkbox"
+          />
+          <span
+            aria-hidden={true}
+            className={clsx(styles.icon, {
+              [styles.indeterminate]: indeterminate && !checked,
+            })}
+          >
+            <svg fill="none" height={16} viewBox="0 0 20 20" width={16}>
+              {checked ? (
+                <CheckedIcon />
+              ) : indeterminate ? (
+                <IndeterminateIcon />
+              ) : null}
+            </svg>
+          </span>
         </span>
         {children && <span className={styles.text}>{children}</span>}
-      </span>
-    </label>
+      </ElementType>
+    </CheckboxContainer>
   );
 };
 
-export default Checkbox;
+export default memo(Checkbox);
