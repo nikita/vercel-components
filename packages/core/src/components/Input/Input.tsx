07f07e65ecba0b90fddc4380384f1ab70b97df51
никита <23018519+nikita@users.noreply.github.com>
2022-08-08 01:30:11 -0700

    refactor(Input): cleanup

--- packages/core/src/components/Input/Input.tsx
+++ packages/core/src/components/Input/Input.tsx
@@ -5,16 +5,10 @@ import { Label } from "@components/Label";
 import { Error } from "@components/Error";
 import { Spacer } from "@components/Spacer";
 import { getThemed } from "@utils/getThemed";
+import { defaultIconSize } from "@utils/iconSize";
 import { useDisabled } from "@contexts/DisabledContext";
 import { IconSizeContext } from "@contexts/IconSizeContext";
 import styles from "./input.module.css";
-
-const defaultSizes = {
-  large: 24,
-  small: 16,
-  default: 20,
-};
-const defaultIconSize = (size) => defaultSizes[size || "default"];
 
 interface Props {
   disabled?: boolean;
@@ -29,7 +23,7 @@ interface Props {
   width?: string;
   error?: string;
   type?: string;
-  size?: string;
+  size?: "small" | "large";
   className?: string;
   wrapperClassName?: string;
   spellCheck?: boolean;
@@ -40,107 +34,118 @@ interface Props {
   id?: any;
 }
 
-const Input = ({
-  disabled,
-  label,
-  typeName = "text",
-  prefix,
-  suffix,
-  prefixStyling = true,
-  prefixContainer = true,
-  suffixStyling = true,
-  suffixContainer = true,
-  width,
-  error,
-  type,
-  size,
-  className,
-  wrapperClassName,
-  spellCheck = false,
-  autoCapitalize = "off",
-  autoComplete = "off",
-  autoCorrect = "off",
-  ...props
-}: Props) => {
-  const Container = label ? Label : React.Fragment;
-  const containerProps = {
-    ...(label && {
-      value: label,
-      withInput: true,
-      style: {
+const Input = React.memo(
+  React.forwardRef(
+    (
+      {
+        disabled,
+        label,
+        typeName = "text",
+        prefix,
+        suffix,
+        prefixStyling = true,
+        prefixContainer = true,
+        suffixStyling = true,
+        suffixContainer = true,
         width,
-      },
-      id: props.id,
-      wrapperClassName,
-    }),
-  };
+        error,
+        type,
+        size,
+        className,
+        wrapperClassName,
+        spellCheck = false,
+        autoCapitalize = "off",
+        autoComplete = "off",
+        autoCorrect = "off",
+        ...props
+      }: Props,
+      ref
+    ) => {
+      const Container = label ? Label : React.Fragment;
+      const containerProps = {
+        ...(label && {
+          value: label,
+          withInput: true,
+          style: {
+            width,
+          },
+          id: props.id,
+          wrapperClassName,
+        }),
+      };
 
-  const ctxDisabled = useDisabled() || disabled;
-  const id = useId("input-");
+      const ctxDisabled = useDisabled() || disabled;
+      const id = useId("input-");
 
-  type = error ? "error" : type;
-  const typeThemed = getThemed(type);
+      type = error ? "error" : type;
+      const typeThemed = getThemed(type);
 
-  return (
-    <Container {...containerProps}>
-      <div
-        className={clsx(styles.container, typeThemed, {
-          [styles().prefix]: prefix,
-          [styles().suffix]: suffix,
-          [styles().noPrefixStyle]: !prefixStyling,
-          [styles().noSuffixStyle]: !suffixStyling,
-          [styles().large]: size === "large",
-          [styles().small]: size === "small",
-        })}
-        data-geist-input-wrapper=""
-        data-version="v1"
-        style={{ width }}
-      >
-        <IconSizeContext.Provider value={defaultIconSize(size)}>
-          <input
-            {...props}
-            aria-describedby={error ? `${id}-error` : props["aria-describedby"]}
-            aria-invalid={!!error}
-            autoCapitalize={autoCapitalize}
-            autoComplete={autoComplete}
-            autoCorrect={autoCorrect}
-            className={clsx(styles.input, className)}
-            data-geist-input=""
-            disabled={ctxDisabled}
-            ref={ref}
-            spellCheck={spellCheck}
-            type={typeName}
-          />
+      return (
+        <Container {...containerProps}>
+          <div
+            className={clsx(styles.container, typeThemed, {
+              [styles.prefix]: prefix,
+              [styles.suffix]: suffix,
+              [styles.noPrefixStyle]: !prefixStyling,
+              [styles.noSuffixStyle]: !suffixStyling,
+              [styles.large]: size === "large",
+              [styles.small]: size === "small",
+            })}
+            data-geist-input-wrapper=""
+            data-version="v1"
+            style={{ width }}
+          >
+            <IconSizeContext.Provider value={defaultIconSize(size)}>
+              <input
+                {...props}
+                aria-describedby={
+                  error ? `${id}-error` : props["aria-describedby"]
+                }
+                aria-invalid={!!error}
+                autoCapitalize={autoCapitalize}
+                autoComplete={autoComplete}
+                autoCorrect={autoCorrect}
+                className={clsx(styles.input, className)}
+                data-geist-input=""
+                disabled={ctxDisabled}
+                // @ts-ignore
+                ref={ref}
+                spellCheck={spellCheck}
+                type={typeName}
+              />
 
-          {prefix &&
-            (prefixContainer ? (
-              <span data-geist-input-prefix="">{prefix}</span>
-            ) : (
-              prefix
-            ))}
+              {prefix &&
+                (prefixContainer ? (
+                  <span data-geist-input-prefix="">{prefix}</span>
+                ) : (
+                  prefix
+                ))}
 
-          {suffix &&
-            (suffixContainer ? (
-              <span data-geist-input-suffix="">{suffix}</span>
-            ) : (
-              suffix
-            ))}
-        </IconSizeContext.Provider>
+              {suffix &&
+                (suffixContainer ? (
+                  <span data-geist-input-suffix="">{suffix}</span>
+                ) : (
+                  suffix
+                ))}
+            </IconSizeContext.Provider>
 
-        {error && (
-          <React.Fragment>
-            <Spacer y={0.5} />
-            <Error
-              /* data-testid={props["data-testid"] ? } */ error={error}
-              id={`${id}-error`}
-              size={size}
-              style={{ width }}
-            />
-          </React.Fragment>
-        )}
-      </div>
-    </Container>
-  );
-};
+            {error && (
+              <React.Fragment>
+                <Spacer y={0.5} />
+                <Error
+                  /* data-testid={props["data-testid"] ? merge(props["data-testid"], "error") : undefined } */
+                  error={error}
+                  id={`${id}-error`}
+                  size={size}
+                  style={{ width }}
+                />
+              </React.Fragment>
+            )}
+          </div>
+        </Container>
+      );
+    }
+  )
+);
 
 export default Input;
