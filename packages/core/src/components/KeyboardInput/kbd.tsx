391e935be7610707e412955c2a303ca76307279f
никита <23018519+nikita@users.noreply.github.com>
2022-08-10 18:16:20 -0700

    refactor(KBD): almost finished updating

--- packages/core/src/components/KeyboardInput/kbd.tsx
+++ packages/core/src/components/KeyboardInput/kbd.tsx
@@ -1,24 +1,43 @@
 import React from "react";
 import clsx from "clsx";
-import { FCC } from "../../react";
-
+import { getThemed } from "@utils/getThemed";
 import styles from "./kbd.module.css";
 
-interface KBDProps {
+interface Props {
   meta?: boolean;
   shift?: boolean;
   alt?: boolean;
   ctrl?: boolean;
   small?: boolean;
+  children?: React.ReactNode;
+  className?: string;
+  type?: string;
 }
 
-const KBD: FCC<KBDProps> = ({ meta, shift, alt, ctrl, small, children }) => {
+// TODO: Finish
+const KBD = ({
+  meta,
+  shift,
+  alt,
+  ctrl,
+  small,
+  children,
+  className,
+  type,
+  ...props
+}: Props) => {
+  const themed = getThemed(type, true);
+
   return (
-    <kbd className={clsx(styles.kbd, { [styles.small]: small })}>
+    <kbd
+      className={clsx(styles.kbd, { [styles.small]: small, themed, className })}
+      data-geist-kbd=""
+      {...props}
+    >
       {meta && <span>⌘</span>}
-      {ctrl && <span>⌃</span>}
       {shift && <span>⇧</span>}
       {alt && <span>⌥</span>}
+      {ctrl && <span>⌃</span>}
       {children && <span>{children}</span>}
     </kbd>
   );
